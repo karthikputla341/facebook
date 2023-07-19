5c3a03c141c3a348a5c4ae7e056430fd5c260db3
karthikputla341 <deepika@gmail.com>
2023-07-19 09:24:59 +0530

    updated

--- face.java
+++ face.java
@@ -1,3 +1,4 @@
 another practice session for git to github linking so be calm 
 this content 
-have a great day
+have a great day i know again i have edited this file 
+note this is for practicing purspose only
